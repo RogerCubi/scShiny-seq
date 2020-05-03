4a7d521f5087e723c01f4f28acfac38267687ff3
Roger Cubi <roger.cubi@hotmail.com>
2020-05-03 19:57:00 +0200

    Added plots of linear dimensional reduction

--- server-dimensionalReduction.R
+++ server-dimensionalReduction.R
@@ -20,3 +20,33 @@ pcaReactive <-
                   }
                 )
 
+
+output$pcaReactive <- reactive({
+  
+  return(!is.null(pcaReactive()))
+  
+})
+outputOptions(output, 'pcaReactive', suspendWhenHidden=FALSE)
+
+
+observe({
+  pcaPlotReactive()
+})
+
+pcaPlotReactive <-
+  eventReactive(input$pcaGraphImput,
+                ignoreNULL = FALSE, {
+                  withProgress(message = "Generating the plot, please wait",{
+                    print("pcaPlotReactive")
+                    
+                    ngsData <- normalizeReactive()$ngsData
+                    
+                    ngsData <- RunPCA(ngsData, features = VariableFeatures(object = ngsData))
+                    
+                    # Examine and visualize PCA results a few different ways
+                    output$renderprint <- renderPrint({print(ngsData[["pca"]], dims = 1:input$PCs_to_Show, nfeatures = input$genes_to_Show)})
+                  })
+                  
+                  return(list("ngsData"=ngsData))  
+                }
+  )
--- ui-tab-dimensionalReduction.R
+++ ui-tab-dimensionalReduction.R
@@ -1,5 +1,6 @@
 tabItem(tabName = "pcaTab",
         
+        # Launch the PCA
         fluidPage(
           sidebarLayout(
             sidebarPanel(
@@ -11,5 +12,29 @@ tabItem(tabName = "pcaTab",
               verbatimTextOutput("renderprint")
               )# mainPanel
           )
-        )
+        ),
+        
+        # Visualization of the PCA
+        
+        conditionalPanel("output.pcaReactive",
+                         fluidPage(
+          sidebarLayout(
+            sidebarPanel(
+              selectInput(inputId = "visualizePCA", label =  "Select PCA visualization",
+                          choices = c("VizDimReduction","DimPlot","DimHeatmap")),
+              conditionalPanel(condition = "input.visualizePCA == 'VizDimReduction'",
+                               numericInput(inputId = "vizdims", label =  "Number of Dimensions to show", value = 2, min = 1, max = Inf, step = 1)
+                               ),
+              conditionalPanel(condition = "input.visualizePCA == 'DimHeatmap'",
+                               numericInput(inputId = "heatdims", label =  "Number of Dimensions to show", value = 6, min = 1, max = Inf, step = 1),
+                               numericInput(inputId = "cellNumber", label =  "Number of Cells to show", value = 500, min = 1, max = Inf, step = 1)
+                               ),
+              hr(),
+              actionButton(inputId = "pcaGraphImput", label = "Validate Selection")
+            ), # sidebarPanel
+            mainPanel(
+              
+            )# mainPanel
+          )
+        ))
 )
