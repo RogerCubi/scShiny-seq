cf1e64e5886c8ed28871d00dca9251d62351c5a9
Roger Cubi <roger.cubi@hotmail.com>
2020-05-10 11:40:18 +0200

    Improved the save and load functions

--- server-saveObject.R
+++ server-saveObject.R
@@ -1,60 +1,15 @@
-# Select folder
-shinyDirChoose(
-  input,
-  'dir',
-  roots = c(home = '~'),
-  filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
+
+# Download rds of analysis
+
+output$downloadData <- downloadHandler(
+  
+  filename = function() {
+    paste(input$projectname, ".rds", sep = "")
+  },
+  content = function(file) {
+    saveRDS(clusteringReactive(), file)
+  }
 )
-
-global <- reactiveValues(datapath = getwd())
-
-dir <- reactive(input$dir)
-
-output$dir <- renderText({
-  global$datapath
-})
-
-observeEvent(ignoreNULL = TRUE,
-             eventExpr = {
-               input$dir
-             },
-             handlerExpr = {
-               if (!"path" %in% names(dir())) return()
-               home <- normalizePath("~")
-               global$datapath <-
-                 file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
-             })
-
-observe({
-  saveObjectReactive()
-})
-
-# Save file
-saveObjectReactive <-
-  eventReactive(input$saveObject,
-                ignoreNULL = FALSE, {
-                  withProgress(message = "Saving the analysis, please wait",{
-                    print("saveObjectReactive")
-                  
-                    ngsData <- clusteringReactive()$ngsData
-                    
-                    # shiny:: validate(
-                    #   need(!(is.null(ngsData)),
-                    #        message = "You need to perform the clustering before saving the analysis")
-                    # )
-                    # 
-                    # shiny:: validate(
-                    #   need(!(is.null(input$filename)),
-                    #        message = "Please, write a name for the file")
-                    # )
-                    saveRDS(ngsData, file = paste0(global$datapath,"/",input$filename,".rds"))
-                    
-                    #output$saveInfo <- renderText(expr = "File saved succesfuly!")
-                    print("Analisys saved")
-                  })
-                  
-                }
-  )
 
 
 observe({
@@ -62,28 +17,23 @@ observe({
 })
 
 # Load file
-loadClusteringReactive <-
-  eventReactive(input$loadObject,
-                ignoreNULL = FALSE, {
-                  withProgress(message = "Loading the analysis file, please wait",{
-                    print("loadObjectReactive")
-
-                    inFile <- input$savedAnalysis
-
-                    if (is.null(inFile))
-                      return(NULL)
-
-                    ngsData <- readRDS(inFile$datapath, refhook = NULL)
-
-
-                    print("Analisys loaded")
-
-                    if (!is.null(ngsData)){
-                      output$loadInfo <- renderText(expr = "File loaded succesfuly!")
-                      }
-                    return(list("ngsData"=ngsData))
-                  })
-
-                }
+loadClusteringReactive <- reactive({
+  withProgress(message = "Loading the analysis file, please wait",{
+    print("loadObjectReactive")
+    
+    inFile <- input$savedAnalysis
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    ngsData <- readRDS(inFile$datapath, refhook = NULL)
+    
+    print("Analisys loaded")
+    
+    if (!is.null(ngsData)){
+      output$loadInfo <- renderText(expr = "File loaded succesfuly!")
+      }
+    return(list("ngsData"=ngsData))
+  })
+  }
   )
-
--- ui-tab-saveObject.R
+++ ui-tab-saveObject.R
@@ -1,29 +1,11 @@
 tabItem(tabName = "saveSeuratTab",
         wellPanel(
-        fluidPage(h4(strong("Save the Seurat Object")),
-          sidebarLayout(
-            sidebarPanel(
-              actionButton(inputId = "saveObject",label = "Save analysis", icon = icon("save") ),
-            ),
-            mainPanel(
-              shinyDirButton("dir", "Input directory", "Upload"),
-              verbatimTextOutput("dir", placeholder = TRUE),
-              hr(),
-              textInput(inputId = "filename", label = "Enter a name for the file"),
-              #textOutput("saveInfo")
-            )# mainPanel
-          )
-        )),
+                fluidPage(h4(strong("Save the Seurat Object")),
+                          conditionalPanel(condition = "!is.null(clusteringReactive()$ngsData)",downloadButton("downloadData", "Download")))),
         wellPanel(
         fluidPage(h4(strong("Load a Seurat Object")),
-                  sidebarLayout(
-                    sidebarPanel(
-                      actionButton(inputId = "loadObject",label = "Load analysis", icon = icon("file-upload")) 
-                    ),
-                    mainPanel(
-                      fileInput(inputId = "savedAnalysis",label = "Select an analysis file",multiple = FALSE),
-                      textOutput("loadInfo")
-                    )# mainPanel
-                  )
-        )),
+                  fileInput(inputId = "savedAnalysis",label = "Select an analysis file",multiple = FALSE),
+                  textOutput("loadInfo")
+                  
+        ))
 )
